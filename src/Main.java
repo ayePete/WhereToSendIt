85dd61cf0ba808ebfa4151693318001b222b17f2
ayePete <petersees4real@gmail.com>
2015-11-21 16:07:12 +0200

    Modified implementation to reflect addition of Journal class.

--- src/Main.java
+++ src/Main.java
@@ -3,45 +3,43 @@ import java.util.*;
 
 public class Main {
 
-    private static final int N = 5;
-    public static ArrayList<Double> acceptanceRates;
-    public static ArrayList<Double> subToPub;
-    public static ArrayList<Double> expectedNumOfCitations;
-    public static ArrayList<String> journalNames;
-    public static List<AbstractMap.SimpleEntry<Integer, Double>> sortedVs;
+    private static final int N = 61;
+    public static ArrayList<Journal> journals;
     public static final double REVISION_TIME = 30;
-    private static final double SCOOP_RATE = 0.001;
-    private static final int TOTAL_TIME = 3000;
+    public static final double SCOOP_RATE = 0.001;
+    public static final int TOTAL_TIME = 3000;
+
 
 
     public static void main(String[] args) {
         System.out.println("Hello this World!");
+
     }
 
-    public static double computeC(){
+    public static double computeC(ArrayList<Journal> journals){
         int q = 1;
         double result = 1 / q;
         double sum = 0;
         for (int j = 1; j <= N; j++) {
             double remainingTimeSum = 0;
             for (int k = 1; k <= j; k++) {
-                remainingTimeSum += subToPub.get(j) - (j - 1) * REVISION_TIME;
+                remainingTimeSum += journals.get(j).getSubToPub() - (j - 1) * REVISION_TIME;
             }
             double resubmissionRiskProduct = 1;
+
             for (int k = 1; k <= j - 1; k++) {
-                resubmissionRiskProduct *= (1 - acceptanceRates.get(k)) * Math.pow(1 - SCOOP_RATE, subToPub.get(k)
+                resubmissionRiskProduct *= (1 - journals.get(k).getAcceptanceRate()) * Math.pow(1 - SCOOP_RATE, journals.get(k).getSubToPub()
                         + REVISION_TIME);
             }
-            sum += acceptanceRates.get(j) * expectedNumOfCitations.get(j)
+            sum += journals.get(j).getAcceptanceRate() * journals.get(j).getExpectedNumOfCitations()
                     * (TOTAL_TIME - remainingTimeSum) * resubmissionRiskProduct;
         }
 
         result *= sum;
         return result;
-
     }
 
-    public static double computeR(){
+    public static double computeR(ArrayList<Journal> journals){
         int q = 1;
         double result = 1 / q;
         double sum = 0;
@@ -50,20 +48,20 @@ public class Main {
             double product = 1;
             double hSum = 0;
             for (int i = 1; i <= j; i++) {
-                hSum += subToPub.get(j) - (j - 1) * REVISION_TIME;
+                hSum += journals.get(j).getSubToPub() - (j - 1) * REVISION_TIME;
             }
             int h = TOTAL_TIME - hSum > 0 ? 1 : 0;
             for (int i = 1; i <= j-1; i++) {
-                product *= (1 - acceptanceRates.get(i)) * Math.pow(1 - SCOOP_RATE, subToPub.get(i)
+                product *= (1 - journals.get(i).getAcceptanceRate()) * Math.pow(1 - SCOOP_RATE, journals.get(i).getSubToPub()
                         + REVISION_TIME)  * h;
             }
-            sum +=  (j+1) * acceptanceRates.get(j) * product;
+            sum +=  (j+1) * journals.get(j).getAcceptanceRate() * product;
         }
         result *= sum;
         return result;
     }
 
-    public static double computeP(){
+    public static double computeP(ArrayList<Journal> journals){
         int q = 1;
         double result = 1 / q;
         double sum = 0;
@@ -72,28 +70,25 @@ public class Main {
             double product = 1;
             double hSum = 0;
             for (int i = 1; i <= j; i++) {
-                hSum += subToPub.get(j) - (j - 1) * REVISION_TIME;
+                hSum += journals.get(i).getSubToPub() - (j - 1) * REVISION_TIME;
             }
             int h = TOTAL_TIME - hSum > 0 ? 1 : 0;
             for (int i = 1; i <= j-1; i++) {
-                product *= (1 - acceptanceRates.get(i)) * Math.pow(1 - SCOOP_RATE, subToPub.get(i)
+                product *= (1 - journals.get(i).getAcceptanceRate()) * Math.pow(1 - SCOOP_RATE, journals.get(i).getSubToPub()
                         + REVISION_TIME)  * h;
             }
-            sum +=  (subToPub.get(j) + (j - 1) * REVISION_TIME) * acceptanceRates.get(j) * product;
+            sum +=  (journals.get(j).getSubToPub() + (j - 1) * REVISION_TIME) * journals.get(j).getAcceptanceRate() * product;
         }
         result *= sum;
         return result;
     }
 
     public static void readData(){
-        acceptanceRates = new ArrayList<>();
-        acceptanceRates.add(-1.0);
-        subToPub = new ArrayList<>();
-        subToPub.add(-1.0);
-        expectedNumOfCitations = new ArrayList<>();
-        expectedNumOfCitations.add(-1.0);
-        journalNames = new ArrayList<>();
-        journalNames.add("Empty");
+        journals = new ArrayList<>();
+        journals.add(new Journal());
+        double acceptanceRate;
+        double subToPub;
+        double expectedNumOfCitations;
 
         FileReader fr;
         try {
@@ -104,16 +99,20 @@ public class Main {
             while(br.ready()){
                 s = br.readLine();
                 String[] sHolder = s.split("\\s+");
-                acceptanceRates.add(Double.parseDouble(sHolder[0]));
-                subToPub.add(Double.parseDouble(sHolder[1]));
-                expectedNumOfCitations.add(Double.parseDouble(sHolder[2]));
+                acceptanceRate = Double.parseDouble(sHolder[0]);
+                subToPub = Double.parseDouble(sHolder[1]);
+                expectedNumOfCitations = Double.parseDouble(sHolder[2]);
+                Journal j = new Journal(acceptanceRate, expectedNumOfCitations, subToPub);
+                journals.add(j);
             }
 
             f = new File("journal_names.txt");
             br = new BufferedReader(new FileReader(f));
+            int i = 1;
             while(br.ready()){
                 s = br.readLine();
-                journalNames.add(s);
+                journals.get(i).setName(s);
+                i++;
             }
 
         } catch (IOException e) {
@@ -122,36 +121,19 @@ public class Main {
 
     }
 
-    /**
-     *
-     * @return Sorted map of v values with index to journal name as key
-     */
-    public static List<AbstractMap.SimpleEntry<Integer, Double>> computeVs(){
-        sortedVs = new ArrayList<>();
-        for (int i = 0; i < journalNames.size(); i++) {
-            double num = acceptanceRates.get(i) * expectedNumOfCitations.get(i)*(1 - subToPub.get(i)/ TOTAL_TIME);
-            double den = 1 - (1 - subToPub.get(i)/ TOTAL_TIME - REVISION_TIME / TOTAL_TIME)
-                    * (1 - acceptanceRates.get(i)) * Math.pow(1 - SCOOP_RATE, REVISION_TIME + subToPub.get(i));
-            double v = num/den;
-            sortedVs.add(new AbstractMap.SimpleEntry<>(i, v));
-        }
-        Collections.sort(sortedVs, new Comparator<AbstractMap.SimpleEntry<Integer, Double>>(){
-
-            public int compare(AbstractMap.SimpleEntry<Integer, Double> o1, AbstractMap.SimpleEntry<Integer, Double> o2){
-                return o1.getValue().compareTo(o2.getValue());
-            }
-        });
-        return sortedVs;
-    }
-
     public static double coEvaluateJournals(int j, int k){
-        double result = acceptanceRates.get(j) * expectedNumOfCitations.get(j) * (TOTAL_TIME - subToPub.get(j)
-                - (1 - acceptanceRates.get(k)) * Math.pow(1 - SCOOP_RATE, REVISION_TIME + subToPub.get(k))
-                * acceptanceRates.get(k) * expectedNumOfCitations.get(k) * (TOTAL_TIME - subToPub.get(k)))
-                + ((1 - acceptanceRates.get(k)) * Math.pow(1 - SCOOP_RATE, REVISION_TIME + subToPub.get(k))
-                * acceptanceRates.get(j) * expectedNumOfCitations.get(j) * (TOTAL_TIME - subToPub.get(k) - REVISION_TIME - subToPub.get(j)));
+        double jRank = journals.get(j).getAcceptanceRate() * journals.get(j).getExpectedNumOfCitations() * (TOTAL_TIME - journals.get(j).getSubToPub()
+                - (1 - journals.get(k).getAcceptanceRate()) * Math.pow(1 - SCOOP_RATE, REVISION_TIME + journals.get(k).getSubToPub())
+                * journals.get(k).getAcceptanceRate() * journals.get(k).getExpectedNumOfCitations() * (TOTAL_TIME - journals.get(k).getSubToPub()))
+                + ((1 - journals.get(k).getAcceptanceRate()) * Math.pow(1 - SCOOP_RATE, REVISION_TIME + journals.get(k).getSubToPub())
+                * journals.get(j).getAcceptanceRate() * journals.get(j).getExpectedNumOfCitations() * (TOTAL_TIME - journals.get(k).getSubToPub() - REVISION_TIME - journals.get(j).getSubToPub()));
+        double kRank = journals.get(k).getAcceptanceRate() * journals.get(k).getExpectedNumOfCitations() * (TOTAL_TIME - journals.get(k).getSubToPub()
+                - (1 - journals.get(j).getAcceptanceRate()) * Math.pow(1 - SCOOP_RATE, REVISION_TIME + journals.get(j).getSubToPub())
+                * journals.get(j).getAcceptanceRate() * journals.get(j).getExpectedNumOfCitations() * (TOTAL_TIME - journals.get(j).getSubToPub()))
+                + ((1 - journals.get(j).getAcceptanceRate()) * Math.pow(1 - SCOOP_RATE, REVISION_TIME + journals.get(j).getSubToPub())
+                * journals.get(j).getAcceptanceRate() * journals.get(k).getExpectedNumOfCitations() * (TOTAL_TIME - journals.get(j).getSubToPub() - REVISION_TIME - journals.get(k).getSubToPub()));
 
-        return result;
+        return jRank >= kRank ? j : k;
     }
 
 }
--- src/Test.java
+++ src/Test.java
@@ -1,14 +1,17 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Collections;
+
 /**
  * Created by Peter on 11/18/2015.
  */
 public class Test {
     public static void main(String[] args){
         Main.readData();
-        System.out.println(Main.journalNames);
-        System.out.println(Main.acceptanceRates);
-        System.out.println(Main.expectedNumOfCitations);
-        System.out.println(Main.subToPub);
-        System.out.println(Main.computeVs());
-        System.out.println(Main.computeC());
+        ArrayList<Journal> journals = (ArrayList<Journal>) Main.journals.clone();
+        System.out.println(journals);
+        System.out.println(Main.computeC(journals));
+        Collections.sort(journals);
+        System.out.println(journals);
     }
 }
